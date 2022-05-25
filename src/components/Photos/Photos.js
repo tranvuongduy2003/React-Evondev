29b6a970484ebe790f48c7db6ccee299e3683149
tranvuongduy2003 <tranvuongduy2003@gmail.com>
2022-05-25 17:12:51 +0700

    button load more

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 // https://picsum.photos/v2/list
+// https://picsum.photos/v2/list?page=2&limit=100
 
-const getRandomPhotos = () => {
+const getRandomPhotos = (page) => {
   return axios
-    .get("https://picsum.photos/v2/list")
+    .get(`https://picsum.photos/v2/list?page=${page}&limit=8`)
     .then((response) => {
       return response.data;
     })
@@ -16,25 +17,45 @@ const getRandomPhotos = () => {
 
 const Photos = () => {
   const [randomPhotos, setRandomPhotos] = useState([]);
-  useEffect(() => {
-    getRandomPhotos().then((images) => {
+  const [nextPage, setNextPage] = useState(1);
+
+  const handleLoadMorePhotos = () => {
+    getRandomPhotos(nextPage).then((images) => {
       console.log(images);
       setRandomPhotos(images);
+      setNextPage(nextPage + 1);
     });
+  };
+
+  useEffect(() => {
+    handleLoadMorePhotos();
   }, []);
 
   return (
-    <div className="grid grid-cols-4 gap-5 p-5">
-      {randomPhotos.length > 0 &&
-        randomPhotos.map((item, index) => (
-          <div key={index} className="p-3 bg-white shadow-md rounded-lg">
-            <img
-              src={item.download_url}
-              alt={item.author}
-              className="w-full h-full object-cover rounded-lg"
-            />
-          </div>
-        ))}
+    <div>
+      <div className="grid grid-cols-4 gap-5 p-5">
+        {randomPhotos.length > 0 &&
+          randomPhotos.map((item, index) => (
+            <div
+              key={index}
+              className="p-3 bg-white shadow-md rounded-lg h-[200px]"
+            >
+              <img
+                src={item.download_url}
+                alt={item.author}
+                className="w-full h-full object-cover rounded-lg"
+              />
+            </div>
+          ))}
+      </div>
+      <div className="text-center">
+        <button
+          onClick={handleLoadMorePhotos}
+          className="inline-block px-8 py-4 bg-purple-600 text-white"
+        >
+          Load more
+        </button>
+      </div>
     </div>
   );
 };
