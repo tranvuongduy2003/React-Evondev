49c816b77318366ac7cbc1c42d6616ad14d1e1a4
tranvuongduy2003 <tranvuongduy2003@gmail.com>
2022-05-26 10:01:07 +0700

    feature loading data

--- src/components/news/HackerNews.js
+++ src/components/news/HackerNews.js
@@ -6,15 +6,19 @@ import axios from "axios";
 const HackerNews = () => {
   const [hits, setHits] = useState([]);
   const [query, setQuery] = useState("react");
+  const [loading, setLoading] = useState("react");
   const handleFetchData = useRef([]);
   handleFetchData.current = async () => {
     try {
+      setLoading(true);
       const response = await axios.get(
         `https://hn.algolia.com/api/v1/search?query=${query}`
       );
       setHits(response.data?.hits || []);
+      setLoading(false);
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   };
   useEffect(() => {
@@ -28,7 +32,11 @@ const HackerNews = () => {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
-      {hits.length &&
+      {loading && (
+        <div className="loading w-8 h-8 rounded-full border border-blue-500 border-4 border-r-white animate-spin"></div>
+      )}
+      {!loading &&
+        hits.length &&
         hits.map((item, index) => <h3 key={item.objectID}>{item.title}</h3>)}
     </div>
   );
