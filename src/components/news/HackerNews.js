ffcb62ee6b3133508847fd658015b9b912189b14
tranvuongduy2003 <tranvuongduy2003@gmail.com>
2022-05-26 10:39:20 +0700

    styling

--- src/components/news/HackerNews.js
+++ src/components/news/HackerNews.js
@@ -31,20 +31,29 @@ const HackerNews = () => {
     handleFetchData.current();
   }, [query]);
   return (
-    <div>
+    <div className="bg-white mx-auto mt-5 mb-5 p-5 rounded-lg shadow-md w-2/4">
       <input
         type="text"
-        className="p-5 mb-5 border border-green-500 text-black"
+        className="border-2 border-gray-200 block w-full rounded-md p-5 mb-5 transition-all focus:border-blue-400 outline-none"
+        placeholder="Typing your key word"
         defaultValue={query}
         onChange={handleUpdateQuery}
       />
       {loading && (
-        <div className="loading w-8 h-8 rounded-full border border-blue-500 border-4 border-r-white animate-spin"></div>
+        <div className="loading w-8 h-8 rounded-full border-blue-500 border-4 border-r-white animate-spin mx-auto my-10"></div>
       )}
-      {!loading && errorMessage && <p>{errorMessage}</p>}
-      {!loading &&
-        hits.length > 0 &&
-        hits.map((item, index) => <h3 key={item.objectID}>{item.title}</h3>)}
+      {!loading && errorMessage && (
+        <p className="text-red-400 my-5">{errorMessage}</p>
+      )}
+      <div className="flex flex-wrap gap-5">
+        {!loading &&
+          hits.length > 0 &&
+          hits.map((item, index) => (
+            <h3 key={item.objectID} className="p-3 bg-gray-100 rounded-md">
+              {item.title}
+            </h3>
+          ))}
+      </div>
     </div>
   );
 };
