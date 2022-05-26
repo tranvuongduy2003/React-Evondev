8307ec31e4d2232ba5e517abd948f9f78f4111d4
tranvuongduy2003 <tranvuongduy2003@gmail.com>
2022-05-26 11:06:19 +0700

    feature fetching button

--- src/components/news/HackerNews.js
+++ src/components/news/HackerNews.js
@@ -10,6 +10,9 @@ const HackerNews = () => {
   const [loading, setLoading] = useState("react");
   const [errorMessage, setErrorMessage] = useState("");
   const handleFetchData = useRef([]);
+  const [url, setUrl] = useState(
+    `https://hn.algolia.com/api/v1/search?query=${query}`
+  );
   handleFetchData.current = async () => {
     try {
       setLoading(true);
@@ -24,21 +27,31 @@ const HackerNews = () => {
       setLoading(false);
     }
   };
-  const handleUpdateQuery = lodash.debounce((e) => {
-    setQuery(e.target.value);
-  }, 500);
+  //   const handleUpdateQuery = lodash.debounce((e) => {
+  //     setQuery(e.target.value);
+  //   }, 500);
   useEffect(() => {
     handleFetchData.current();
-  }, [query]);
+  }, [url]);
   return (
     <div className="bg-white mx-auto mt-5 mb-5 p-5 rounded-lg shadow-md w-2/4">
-      <input
-        type="text"
-        className="border-2 border-gray-200 block w-full rounded-md p-5 mb-5 transition-all focus:border-blue-400 outline-none"
-        placeholder="Typing your key word"
-        defaultValue={query}
-        onChange={handleUpdateQuery}
-      />
+      <div className="flex mb-5 gap-x-5">
+        <input
+          type="text"
+          className="border-2 border-gray-200 block w-full rounded-md p-5 transition-all focus:border-blue-400 outline-none"
+          placeholder="Typing your key word"
+          defaultValue={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button
+          onClick={() =>
+            setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
+          }
+          className="bg-blue-500 text-white font-semibold p-5 rounded-md flex-shrink-0"
+        >
+          Fetching
+        </button>
+      </div>
       {loading && (
         <div className="loading w-8 h-8 rounded-full border-blue-500 border-4 border-r-white animate-spin mx-auto my-10"></div>
       )}
@@ -48,11 +61,14 @@ const HackerNews = () => {
       <div className="flex flex-wrap gap-5">
         {!loading &&
           hits.length > 0 &&
-          hits.map((item, index) => (
-            <h3 key={item.objectID} className="p-3 bg-gray-100 rounded-md">
-              {item.title}
-            </h3>
-          ))}
+          hits.map((item, index) => {
+            if (!item.title) return null;
+            return (
+              <h3 key={item.objectID} className="p-3 bg-gray-100 rounded-md">
+                {item.title}
+              </h3>
+            );
+          })}
       </div>
     </div>
   );
