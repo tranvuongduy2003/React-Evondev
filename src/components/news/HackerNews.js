7dabbe2d9f4cc3a2f0a6093701c577541dbcd8b7
tranvuongduy2003 <tranvuongduy2003@gmail.com>
2022-05-26 10:12:36 +0700

    feature error message

--- src/components/news/HackerNews.js
+++ src/components/news/HackerNews.js
@@ -7,17 +7,19 @@ const HackerNews = () => {
   const [hits, setHits] = useState([]);
   const [query, setQuery] = useState("react");
   const [loading, setLoading] = useState("react");
+  const [errorMessage, setErrorMessage] = useState("");
   const handleFetchData = useRef([]);
   handleFetchData.current = async () => {
     try {
       setLoading(true);
       const response = await axios.get(
-        `https://hn.algolia.com/api/v1/search?query=${query}`
+        `https://hn.algolia.com/api/v1/search?qery=${query}`
       );
       setHits(response.data?.hits || []);
       setLoading(false);
     } catch (error) {
       console.log(error);
+      setErrorMessage(`The error happened ${error}`);
       setLoading(false);
     }
   };
@@ -35,8 +37,9 @@ const HackerNews = () => {
       {loading && (
         <div className="loading w-8 h-8 rounded-full border border-blue-500 border-4 border-r-white animate-spin"></div>
       )}
+      {!loading && errorMessage && <p>{errorMessage}</p>}
       {!loading &&
-        hits.length &&
+        hits.length > 0 &&
         hits.map((item, index) => <h3 key={item.objectID}>{item.title}</h3>)}
     </div>
   );
