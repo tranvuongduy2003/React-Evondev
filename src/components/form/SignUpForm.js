ffabe41ef99641a4bf08a6aaf402808e8a49326d
tranvuongduy2003 <tranvuongduy2003@gmail.com>
2022-05-28 23:18:43 +0700

    SignUpForm validation

--- src/components/form/SignUpForm.js
+++ src/components/form/SignUpForm.js
@@ -1,18 +1,79 @@
 import React from "react";
+import { useFormik } from "formik";
+
+const validate = (values) => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  } else if (values.firstName.length > 20) {
+    errors.firstName = "Must be 20 characters or less";
+  }
+
+  if (!values.lastName) {
+    errors.lastName = "Required";
+  } else if (values.lastName.length > 20) {
+    errors.lastName = "Must be 20 characters or less";
+  }
+
+  return errors;
+};
 
 const SignUpForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
-    <div className="p-10 w-full max-w-[500px] mx-auto">
-      <div className="flex flex-col gap-2">
-        <label htmlFor="firstname">Firstname</label>
+    <form
+      onSubmit={formik.handleSubmit}
+      className="p-10 w-full max-w-[500px] mx-auto"
+      autoComplete="off"
+    >
+      <div className="flex flex-col gap-2 mb-5">
+        <label htmlFor="firstname">First name</label>
         <input
           type="text"
-          id="firstname"
+          id="firstName"
+          name="firstName"
           placeholder="Enter your first name"
           className="p-4 rounded-md border border-gray-100 outline-none"
+          value={formik.values.firstName}
+          onChange={formik.handleChange}
         />
+        {formik.errors.firstName ? (
+          <div className="text-sm text-red-500">{formik.errors.firstName}</div>
+        ) : null}
       </div>
-    </div>
+      <div className="flex flex-col gap-2 mb-5">
+        <label htmlFor="lastName">Last name</label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          placeholder="Enter your last name"
+          className="p-4 rounded-md border border-gray-100 outline-none"
+          value={formik.values.lastName}
+          onChange={formik.handleChange}
+        />
+        {formik.errors.lastName ? (
+          <div className="text-sm text-red-500">{formik.errors.lastName}</div>
+        ) : null}
+      </div>
+      <div>
+        <button
+          type="submit"
+          className="w-full p-4 bg-blue-500 text-white font-semibold rounded-lg"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
   );
 };
 
