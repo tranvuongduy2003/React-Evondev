89143ffd389082356e0f41dfa86ef7963eaac683
tranvuongduy2003 <tranvuongduy2003@gmail.com>
2022-05-28 20:50:39 +0700

    MovieSearchApp search feature

--- src/components/MovieSearchApp/MovieSearchApp.js
+++ src/components/MovieSearchApp/MovieSearchApp.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { debounce } from "lodash";
 import React, { useEffect, useState } from "react";
 
 // https://api.themoviedb.org/3/movie/550?api_key=7e12d3869fc93c0d942c505c589fe77a
@@ -6,12 +7,13 @@ import React, { useEffect, useState } from "react";
 
 const MovieSearchApp = () => {
   const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await axios.get(
-          `https://api.themoviedb.org/3/search/movie?api_key=7e12d3869fc93c0d942c505c589fe77a&query=''`
+          `https://api.themoviedb.org/3/search/movie?api_key=7e12d3869fc93c0d942c505c589fe77a&query='${query}'`
         );
         setMovies(response?.data?.results || []);
       } catch (error) {
@@ -19,7 +21,7 @@ const MovieSearchApp = () => {
       }
     }
     fetchData();
-  }, []);
+  }, [query]);
 
   return (
     <div className="p-10">
@@ -28,6 +30,7 @@ const MovieSearchApp = () => {
           type="text"
           className="w-full p-5 rounded-lg shadow-[0px_0px_0px_3px_rgba(125,_106,_255,_0.2)] border-2 border-gray-400 focus:border-purple-500 outline-none"
           placeholder="Search movie"
+          onChange={(e) => debounce(setQuery(e.target.value), 1000)}
         />
       </div>
       <div className="grid grid-cols-3 gap-10">
@@ -42,7 +45,7 @@ const MovieSearchApp = () => {
 
 const MovieItem = ({ data }) => {
   return (
-    <div className="bg-white p-3 rounded-2xl shadow-sm">
+    <div className="bg-white p-3 rounded-2xl shadow-sm flex flex-col">
       <div className="h-[297px]">
         <img
           className="h-full w-full object-cover rounded-lg"
@@ -50,12 +53,14 @@ const MovieItem = ({ data }) => {
           alt=""
         />
       </div>
-      <div className="p-7">
+      <div className="p-7 flex-1 flex flex-col">
         <h3 className="text-lg text-black font-semibold mb-4">
           {data.original_title}
         </h3>
-        <p className="text-[#999] text-sm mb-6">{data.overview}</p>
-        <div className="flex items-center gap-x-3">
+        <p className="text-[#999] text-sm mb-6 !leading-loose">
+          {data.overview}
+        </p>
+        <div className="flex items-center gap-x-3 mt-auto">
           <img src="./star.svg" alt="" />
           <span className="text-sm font-semibold text-[#333]">
             {data.vote_average}
